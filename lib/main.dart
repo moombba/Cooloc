7afdc571fc4d33400a091a651c045165802119b9
Quentin Claudet <q.claudet@gmail.com>
2023-12-14 14:29:10 +0100

    Update main.dart with system orientation lock

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,15 @@ import 'package:afgf_front/pages/main_page.dart';
 import 'package:afgf_front/theme/dark_theme.dart';
 import 'package:afgf_front/theme/light_theme.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/services.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp( const ProviderScope(child: MyApp()));
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((_) {
+     runApp(const ProviderScope(child: MyApp()));
+  });
+
 }
 
 class MyApp extends StatelessWidget {
